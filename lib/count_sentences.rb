c442d2b7ec13d3933a8923297dbd089012776474
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-30 22:42:29 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+	  self.end_with?("?")
   end
 
   def exclamation?
-
+	 self.end_with?("!")
   end
-
   def count_sentences
-
+	 #convert to array of words
+	words= self.split()
+	count=0;
+    #check if each word ends with . or ? or !
+	words.each {|word|
+	  if word.end_with?("?")||word.end_with?("!")||word.end_with?(".")
+		count+=1
+	  end
+		  }
+	  count
   end
 end
+	  
+	  
+	

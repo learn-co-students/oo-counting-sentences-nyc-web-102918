7e4586c2ed08d931c9acd01ad328398c13ac0ee3
liuh <hanke.liu@gmail.com>
2018-10-31 18:04:48 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
+
+    self.split(/[.?!]/).reject(&:empty?).count 
+#bingind.pry
+    #split String
+    #count the splitted string
+    #remove empty strings
+    #iterate through each and flatten the array, do it again.
 
   end
 end

f57a9c0bbcfb6581ad1f98d3cede090140b7954a
jbmenashi <jbmenashi@gmail.com>
2018-10-30 20:00:39 -0400

    passed all tests

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    word_array = self.split(" ")
+    sentence_array = word_array.select do |word|
+      word.sentence? || word.question? || word.exclamation?
+    end 
+    sentence_array.length
   end
 end
